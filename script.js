cf08b227a8a24be43760056f9de6095b9bc9a538
Your name here <kr.shampa@yahoo.com>
2020-03-07 16:10:04 -0600

    adding new cat

---
+++ script.js
@@ -0,0 +1,7 @@
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-generator');
+    image.src = "https://cdn2.thecatapi.com/images/LSaDk6OjY.jpg";
+    div.append(image);
+    
+}
